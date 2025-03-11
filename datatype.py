13d005969d64fdc8e1415fff41977d8624b45717
karthik <karthi98karthik19@gmail.com>
2025-03-11 17:46:59 +0000

    new

---
+++ datatype.py
@@ -0,0 +1,9 @@
+  #data types 
+second_name = "vijay"
+print(second_name+"is Years old  ")
+print("55555" + "10")
+print("10" + "10")
+print(10-10)
+#bollean section  
+ismale = True
+print(ismale)
---
+++ string.py
@@ -0,0 +1,3 @@
+string = " my name"
+print(string + "is karthik ")
+ 
